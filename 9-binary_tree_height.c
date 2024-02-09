3204101a0bfbcb2ab18d3fab6b6fcf9157cdad3d
maazalside <maazalside@gmail.com>
2024-02-09 16:41:06 +0300

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,18 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	else
+		left_height = 0;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	else
+		right_height = 0;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return ((left_height > right_height) ? 1 : right_height);
 }
